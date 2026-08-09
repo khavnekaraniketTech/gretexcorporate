e18b079fd4d34b0215886e063bd2ffe6433f331e
khavnekaraniketTech <aniketkhavnekar.tech@gmail.com>
2026-08-09 21:44:33 +0530

    error for Express backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 const rateLimit = require('express-rate-limit');
 const { body, validationResult } = require('express-validator');
 
-const Contact = require('./models/Contact');
+const Contact = require('./models/Contact.js');
 
 dotenv.config();
 
